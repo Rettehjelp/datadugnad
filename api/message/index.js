74df62b1b33845f4cdf0da2ddea0eacc09d1ae23
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2023-11-16 12:28:14 +0000

    funker

--- api/message/index.js
+++ api/message/index.js
@@ -6,8 +6,21 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        context.res.json({
+            text: "Hello from the API"
+        });
+    
+
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: {
+    //         msg: responseMessage
+    //     } 
+    // };
 }
+
+module.exports = async function (context, req) {
+    context.res.json({
+        text: "Hello from the API"
+    });
+};
